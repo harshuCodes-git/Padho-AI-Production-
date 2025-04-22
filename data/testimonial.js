2d66afd9e9323201c13560854ffd2e6da7ecc45a
harshuCodes-git <1032210414@tcetmumbai.in>
2025-04-22 22:19:13 +0530

    Update testimonial.js

--- data/testimonial.js
+++ data/testimonial.js
@@ -5,25 +5,25 @@ export const testimonial = [
     author: "Harsh Agarwal",
     image:
       "https://media-hosting.imagekit.io/82a0f644aa77497b/WhatsApp Image 2025-04-22 at 21.23.00_3042db81.jpg?Expires=1839945267&Key-Pair-Id=K2ZIVPTIP2VGHC&Signature=qcW-8EQaUiOo-66~yfWObRKPzWfd45rzCvNazzn5rX4bqDI2v7coVNsyd-u0ZeOXE~IQ7Ljmc2rgB6nUkf1jh3HoTYGs5SLZ8LF3GoATVqzXsTAsZCj78KMZrwakeXErAQGQEzyTqyKcpmLMluDiBMBXINGcN9fqKY0DHAydn09uv1u3BymhuA5EiFinP5q39xWg7BBmtxdSkIKEEDAvuIxOrvAylAzrgUhQCTlACEKc-oOiCAgLbiYcbHLh17At9fMpmtW8skNsdhC67ZZ4upfBmrMtNrcvu0caNNUrvtaHN4SMpvhsZ7ElU-WO4O-WR8G8pBDuPa8v9BjiHlhO4w__",
-    role: "Software Engineer",
-    company: "Tech Solutions",
+    role: "Research Analyst",
+    company: "Padho AI",
   },
   {
     quote:
-      "The industry insights helped me pivot my career successfully. The salary data was spot-on!",
-    author: "Michael Rodriguez",
+      "The industry insights helped me pivot my career successfully. ",
+    author: "Harsh Dubey",
     image:
       "https://media-hosting.imagekit.io/da6e658b2f884afc/profile_pic_new.jpg?Expires=1839945597&Key-Pair-Id=K2ZIVPTIP2VGHC&Signature=onT-0jU92MhHcAazltbVlTsWfpW8ycXq-ckh8U5znpjlR8qBxPYKqKu0krbnZuDJ5IZoN~QDEKewItVig4i5E3B3NhrV3P~a7rFcqFpOM675wmNIKoxELqu-KsQsU2nlareUdA3h0SFsii6bt~Ep3PVLILFrEnYP-bsF55pFzGtIO3xG2riVZt2EaCqdNF7W1EtNecdfyWQkSCc~SxDeqt8MYoA2BEt4gOzlBfxMw9x9xdfs-wtRLkjT~tO~cdMJcHQx7sG6AUND2KDS3k2eqjV3suBV21~v-oS~uJZEjvEhRm20Pqa3pJb-xVbKK1dcwEgQbu9R3C5qbB9slrnzfg__",
-    role: "Product Manager",
-    company: "StartUp Inc.",
+    role: " Lead Developer",
+    company: "Padho AI",
   },
   {
     quote:
-      "My resume's ATS score improved significantly. Got more interviews in two weeks than in six months!",
-    author: "Priya Patel",
+      "Got more interviews in two weeks than in six months!",
+    author: "Mukesh Gupta",
     image:
       "https://media-hosting.imagekit.io/3b3aed2dcbe246f2/WhatsApp%20Image%202025-04-22%20at%2021.29.44_54fa4bd1.jpg?Expires=1839945682&Key-Pair-Id=K2ZIVPTIP2VGHC&Signature=ARtdNJT0rEl5ymbEaMwJxXnpa8gBqNxysfMlDpPNnNuMuboEzQsav7frJ0K4NzJRh0nk5IidHpr~FikRSGaoxgv3lh0lIJOmAVEwql18x3vVh8EDET-VUMlSPY6ZBXpwjl2-GjPK8ISZX~4QMcIfQEgK6Bo2bSNyuLvO41zZzNungo6j428dApVXzwlTpqnzOazwEXBhq4QtTYsxDnmvCqIjGGxZzP3AQawvboA6SRX-QIADg-q35hq5MHcrLfNargJexifJ6XYLxvfsnkuh~or-~u45FsLEYD7Vs8e83x6a935RxMzqoSL4zRRtSpQhrknv-o~59eE7PWcR33tuHA__",
-    role: "Marketing Director",
-    company: "Global Corp",
+    role: "Developer",
+    company: "Padho AI",
   },
 ];
